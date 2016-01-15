ff1d36d272d16f2805302f8f3ebeffac6dc7dc00
Yuzhong Wen <wyz2014@vt.edu>
2016-01-15 15:15:06 -0500

    Add poll support

---
+++ fs/ft_select.c
@@ -0,0 +1,103 @@
+/*
+ * ft_select.c
+ * Copyright (C) 2016 Yuzhong Wen <wyz2014@vt.edu>
+ *
+ * This is for replicating select and poll system calls
+ */
+
+#include <linux/kernel.h>
+#include <linux/file.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/mm.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
+#include <linux/string.h>
+#include <linux/poll.h>
+#include <asm/uaccess.h>
+#include <linux/list.h>
+#include <linux/ft_replication.h>
+
+/*
+ * Structure for poll_wait syscall info
+ */
+struct poll_wait_info {
+	int nr_events;     // Number of events
+	struct pollfd events[0];   // Array of events, it has to be at the end of the struct
+} __attribute__((packed));
+
+
+/*
+ * Structure for select_wait syscall info
+ */
+struct select_wait_info {
+} __attribute__((packed));
+
+/*
+ * Send the poll info to the other side
+ */
+int ft_poll_primary(struct pollfd __user *events, int nr_events)
+{
+	struct poll_wait_info *pinfo = NULL;
+	ssize_t pinfo_size;
+
+	if (nr_events > 0) {
+		pinfo_size = sizeof(struct poll_wait_info) + nr_events * sizeof(struct pollfd);
+	} else {
+		pinfo_size = sizeof(struct poll_wait_info);
+	}
+	pinfo = (struct poll_wait_info *) kmalloc(pinfo_size, GFP_KERNEL);
+
+	if (!pinfo) {
+		printk("epinfo allocation failed!\n");
+		return -ENOMEM;
+	}
+
+	pinfo->nr_events = nr_events;
+
+	if (nr_events > 0) {
+		copy_from_user(pinfo->events, events, nr_events * sizeof(struct pollfd));
+	}
+	
+	if(is_there_any_secondary_replica(current->ft_popcorn)){
+		printk("sending %d events for syscall %d %d\n", nr_events, current->id_syscall, current->pid);
+		ft_send_syscall_info(current->ft_popcorn, &current->ft_pid, current->id_syscall, (char*) pinfo, pinfo_size);
+	}
+
+	kfree(pinfo);
+
+	return 0;
+}
+
+/*
+ * Wait for the poll info from the other side
+ */
+int ft_poll_secondary(struct pollfd __user *events)
+{
+	int ret=0;
+	int i;
+	struct poll_wait_info *pinfo = NULL;
+
+	printk("waiting events for syscall %d %d\n", current->id_syscall, current->pid);
+	pinfo = (struct poll_wait_info *) ft_wait_for_syscall_info(&current->ft_pid, current->id_syscall);
+
+	if (!pinfo) {
+		return -EFAULT;
+	}
+
+	if (pinfo->nr_events > 0) {
+		for (i = 0; i < pinfo->nr_events; i++) {
+			pinfo->events[i].fd -= 3;
+		}
+		copy_to_user(events, pinfo->events, pinfo->nr_events * sizeof(struct pollfd));
+	} else {
+		printk("OOPS %d\n", pinfo->nr_events);
+	}
+
+	ret = pinfo->nr_events;
+	printk("got %d events\n", ret);
+	kfree(pinfo);
+
+	return ret;
+
+}
